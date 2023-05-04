ebd5d6a512642e7ac0019a84d93e32e5f37520c4
Kevin F <Freymaurer@gmx.de>
2023-05-04 14:01:19 +0200

    finish io bindings (untested)

--- src/Fable.exceljs/Csv.fs
+++ src/Fable.exceljs/Csv.fs
@@ -1,16 +1,12 @@
 ﻿[<AutoOpen>]
-module Fable.ExcelJs.Xlsx
+module Fable.ExcelJs.Csv
 
-type Xlsx =
+type Csv =
     /// read from a file
     abstract member readFile: filename:string -> Async<unit>
     /// read from a stream
     abstract member read: filename:System.IO.Stream -> Async<unit>
-    /// load from a buffer
-    abstract member load: filename:obj -> Async<unit>
     /// write to a file
     abstract member writeFile: filename:string -> Async<unit>
     /// write to a stream
-    abstract member write: filename:System.IO.Stream -> Async<unit>
-    /// write to a new buffer
-    abstract member writeBuffer: unit -> Async<obj>
+    abstract member write: filename:System.IO.Stream * {|sheetName: string|} -> Async<unit>
