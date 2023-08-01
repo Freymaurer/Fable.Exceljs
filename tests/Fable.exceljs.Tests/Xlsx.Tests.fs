881e7fb81b641776087944a03aef56f6dac8cf03
Kevin F <Freymaurer@gmx.de>
2023-08-01 22:10:04 +0200

    Make xlsx functions correctly Promise instead of Async :tada:

--- tests/Fable.exceljs.Tests/Xlsx.Tests.fs
+++ tests/Fable.exceljs.Tests/Xlsx.Tests.fs
@@ -1,16 +1,14 @@
 ﻿module Xlsx.Tests
 
-open Fable.Core.JsInterop
 open Fable.Mocha
 open Fable.Core
-open Fable.Core.JS
-open Fable.Core.JsInterop
 open Fable.ExcelJs
 
 module TestWorkbooks =
     
-    [<Literal>]
-    let private ws_name = "MySheet1"
+    let [<Literal>] ws_name = "MySheet1"
+
+    let [<Literal>] table_name = "MyTable"
 
     let Workbook1 =
         let wb = ExcelJs.Excel.Workbook()
@@ -26,10 +24,37 @@ module TestWorkbooks =
                     yield
                         [|box $"Row {i}"; box i; (i%2 |> fun x -> x = 1 |> box)|]
             |]
-        let table_t = Table("MyTable","A1",cols,rows)
+        let table_t = Table(table_name,"A1",cols,rows)
         let table = ws.addTable(table_t)
         wb
 
+let [<Literal>] File1Path = @"C:\Users\Kevin\source\repos\Fable.Exceljs\tests\Fable.Exceljs.JsNativeTests/ReadWriteFableTest.xlsx"
+
+let tests_write = testList "write" [
+    testAsync "ensure async" {
+        do! Async.Sleep 300
+        Expect.isTrue true ""
+    }
+    testAsync "write file" {
+        do! TestWorkbooks.Workbook1.xlsx.writeFile File1Path |> Async.AwaitPromise
+        Expect.isTrue true ""
+    }  
+]
+
+let tests_read = testList "read" [
+    testAsync "read file" {
+        let wb = ExcelJs.Excel.Workbook()
+        do! wb.xlsx.readFile File1Path |> Async.AwaitPromise
+        let ws = wb.getWorksheet(TestWorkbooks.ws_name)
+        Expect.equal ws.name TestWorkbooks.ws_name "ws.name"
+        let table = ws.getTable(TestWorkbooks.table_name)
+        Expect.equal table.name TestWorkbooks.table_name "table.name"
+    }  
+]
+
 /// https://github.com/Zaid-Ajaj/Fable.Mocha/issues/69
-let main = testList "Xlsx" [ ]
+let main = testSequenced <| testList "Xlsx" [ 
+    tests_write
+    tests_read
+]
 
